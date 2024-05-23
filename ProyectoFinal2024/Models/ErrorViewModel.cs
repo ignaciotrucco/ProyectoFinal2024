f0653287643157e5c443a72cb17362dbceaf9133
ignaciotrucco <truccoignacio9@gmail.com>
2024-05-23 17:19:44 -0300

    primer commir

---
+++ ProyectoFinal2024/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoFinal2024.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
